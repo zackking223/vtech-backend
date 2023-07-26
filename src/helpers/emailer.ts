132de961c547ff17995098fe87cb36259487d7dd
Codenoob223 <vuongdepzai1262003@gmail.com>
2023-07-26 15:57:11 +0700

    Delete files

--- src/helpers/emailer.ts
+++ src/helpers/emailer.ts
@@ -9,7 +9,7 @@ export const emailer = (from : string, to : string, protocol : string, host : st
   // create reusable transporter object using the default SMTP transport
   //Nodemailer configuration
   const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
+    host: 'smtp-relay.brevo.com',
     port: 587,
     auth: {
         user: process.env.ETHEREAL_USER,
